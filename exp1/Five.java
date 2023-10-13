686e6cf45cf47f94c95465bef6722b051ae4810b
Sujal <sjlchoudhari@gmail.com>
2023-10-13 03:29:59 +0000

    Added exp1 Q5

---
+++ exp1/Five.java
@@ -0,0 +1,75 @@
+import java.util.*;
+/*
+ * Write a java program that maintains a separate linked list denoting marks of students from two divisions. Perform the following operations on Linked List: 
+
+    Merge both the list 
+
+    Sort 
+
+    Minimum and maximum 
+
+    Split failed and passed  
+
+    Count number of students getting above 90 marks 
+ */
+
+public class Five {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        linkedList.add(10);
+        linkedList.add(20);
+        linkedList.add(40);
+        linkedList.add(90);
+        linkedList.add(98);
+        linkedList.add(94);
+        linkedList.add(33);
+
+        LinkedList<Integer> linkedList2 = new LinkedList<Integer>();
+        linkedList2.add(15);
+        linkedList2.add(25);
+        linkedList2.add(45);
+        linkedList2.add(95);
+        linkedList2.add(95);
+        linkedList2.add(95);
+        linkedList2.add(35);
+
+        // Merge
+        LinkedList<Integer> mergedList = (LinkedList<Integer>) linkedList.clone();
+        mergedList.addAll(linkedList2);
+
+        System.out.println(linkedList);
+        System.out.println(linkedList2);
+        System.out.println(mergedList);
+
+        Collections.sort(mergedList);
+
+        System.out.println("Min:" + mergedList.peekFirst());
+        System.out.println("Max:" + mergedList.peekLast());
+
+        LinkedList<Integer> passedCandidates = new LinkedList<Integer>();
+        LinkedList<Integer> failedCandidates = new LinkedList<Integer>();
+
+        for (int i : mergedList) {
+            if (i < 40)
+                failedCandidates.add(i);
+            else
+                passedCandidates.add(i);
+        }
+
+        System.out.println("Passed:" + passedCandidates);
+        System.out.println("Failed:" + failedCandidates);
+
+
+        LinkedList<Integer> toppers =  new LinkedList<Integer>();
+        for(int i: mergedList){
+            if(i >90)
+            toppers.add(i);
+        }
+        
+
+        System.out.println("Num of Toppers: " + toppers.size());
+
+
+
+    }
+}
