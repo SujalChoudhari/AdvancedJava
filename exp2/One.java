4a95fc25d6a5b90919c57022a4382b1bc1dd9adf
Sujal <sjlchoudhari@gmail.com>
2023-10-20 03:35:38 +0000

    Exp2 Two using Array

--- exp2/One.java
+++ exp2/One.java
@@ -1,40 +1,40 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class One {
 
     public static Scanner sc = new Scanner(System.in);
 
-    public static <E> void printArray(ArrayList<E> array) {
-        System.out.println("Printing: " + array.get(0).getClass().getName());
+    public static <E> void printArray(E[] array) {
+        System.out.println("Printing: " + array[0].getClass().getName());
         for (E item : array) {
             System.out.println(item);
         }
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> intArray = new ArrayList<Integer>();
-        ArrayList<Double> doubleArray = new ArrayList<Double>();
-        ArrayList<String> strArray = new ArrayList<String>();
+        Integer[] intArray = new Integer[4];
+        Double[] doubleArray = new Double[4];
+        String[] strArray = new String[4];
 
         int count = 0;
         System.out.println("Enter int data:");
         while (count < 4) {
-            intArray.add(sc.nextInt());
+            intArray[count] = sc.nextInt();
             count++;
         }
 
         count = 0;
         System.out.println("Enter double data:");
         while (count < 4) {
-            doubleArray.add(Double.parseDouble(sc.nextLine()));
+            doubleArray[count] = sc.nextDouble();
             count++;
         }
 
         count = 0;
         System.out.println("Enter str data:");
         while (count < 4) {
-            strArray.add(sc.nextLine());
+            sc.next();
+            strArray[count] = sc.nextLine();
             count++;
         }
 
